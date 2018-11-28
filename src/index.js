424013fe34f5a7a3cd1f733b549c7b3b381aa3a6
lyuzhinan <liuliu911@gmail.com>
2018-11-28 16:36:18 +0800

    CommentApp

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import CommentApp from './CommentApp';
 import Home from './Home';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+// ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<CommentApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
